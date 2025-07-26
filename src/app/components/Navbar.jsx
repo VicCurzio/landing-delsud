1661efb37467088dc74747a99cf0ffa52948d07d
Victor Roberto Curzio <victor.curzio@hotmail.com>
2025-07-26 16:34:40 -0300

    navbar mobile in progress

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,35 +1,51 @@
 'use client';
 
-import Link from 'next/link';
 import { useState } from 'react';
 
+import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
-    const [activeLang, setActiveLang] = useState('');
+    const [activeLang, setActiveLang] = useState('ES');
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleLangClick = (lang) => {
         setActiveLang(lang);
+        // Opcional: Si clickea un idioma, cierra el menú móvil
+        if (isMenuOpen) {
+            setIsMenuOpen(false);
+        }
+    };
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
     };
 
     return (
         <header className={styles['navbar-container']}>
-            <div className={styles['navbar-logo']}>
+            <div className={styles['navbar-logo-wrapper']}>
                 <div className={styles['logo-rectangle']}></div>
                 <h1>LOGO EMPRESA</h1>
             </div>
-            <nav className={styles['navbar-nav']}>
+
+            <button className={styles['hamburger-menu']} onClick={toggleMenu} aria-expanded={isMenuOpen} aria-controls="mobile-nav-items">
+                <span className={styles['hamburger-icon']}></span>
+                <span className={styles['hamburger-icon']}></span>
+                <span className={styles['hamburger-icon']}></span>
+            </button>
+
+            <nav className={`${styles['navbar-nav']} ${isMenuOpen ? styles['navbar-nav-open'] : ''}`} id="mobile-nav-items">
                 <ul className={styles['navbar-menu']}>
-                    <li><Link href="#">Item 1</Link></li>
-                    <li><Link href="#">Item 2</Link></li>
-                    <li><Link href="#">Item 3</Link></li>
+                    <li><Link href="#" onClick={() => setIsMenuOpen(false)}>Item 1</Link></li>
+                    <li><Link href="#" onClick={() => setIsMenuOpen(false)}>Item 2</Link></li>
+                    <li><Link href="#" onClick={() => setIsMenuOpen(false)}>Item 3</Link></li>
                 </ul>
                 <ul className={styles['navbar-lang-switcher']}>
                     <li>
                         <Link
                             href="#"
                             onClick={() => handleLangClick('EN')}
-                            className={`${activeLang === 'EN' ? styles['current-lang-active'] : ''}`}
+                            className={`${activeLang === 'EN' ? styles['lang-selected'] : ''} ${activeLang === 'EN' ? styles['current-lang-active'] : ''}`}
                         >
                             EN
                         </Link>
